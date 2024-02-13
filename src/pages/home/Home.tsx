ce7dc94f832fb6dc02652dd95078b4819868d31d
jhael07fmp <jhrodriguez@fmp.com.do>
2024-02-13 17:42:29 -0400

    template done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,66 +1,74 @@
-import InputCustom from "../../components/Form/Inputs/InputCustom";
-import { useForm } from "react-hook-form";
+import React from "react";
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import Navbar from "../../components/layout/Navbar";
 
-const Home = () => {
-  const { register, setValue } = useForm();
+const { Content, Sider } = Layout;
+
+const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps["items"] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
+
+const Home: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   return (
-    <div className="text-2xl p-10 grid gap-6 mx-auto w-fit grid-cols-2">
-      <InputCustom
-        label="Date"
-        placeholder="Type your name"
-        name="date"
-        setValue={setValue}
-        register={register}
-        type="date"
-      />
-      <InputCustom
-        label="DateTime"
-        placeholder="Type your name"
-        name="datetime"
-        setValue={setValue}
-        register={register}
-        type="datetime"
-      />
-      <InputCustom
-        label="Select"
-        placeholder="Type your name"
-        name="select"
-        register={register}
-        type="select"
-        setValue={setValue}
-        options={[{ label: "option 1", value: 1 }]}
-      />
-      <InputCustom
-        label="Radius"
-        placeholder="Type your name"
-        name="select"
-        register={register}
-        type="radius"
-      />
-      <InputCustom label="text" placeholder="Type your name" name="text" register={register} />
-      <InputCustom
-        label="Number"
-        placeholder="Type your name"
-        type="number"
-        name="number"
-        register={register}
-      />
-      <InputCustom
-        label="Password"
-        placeholder="Type your name"
-        name="password"
-        type="password"
-        register={register}
-      />
-      <InputCustom
-        label="Password"
-        placeholder="Type your name"
-        name="password"
-        type="upload"
-        register={register}
-      />
-    </div>
+    <>
+      <Navbar items={items1} />
+      <Layout>
+        <Sider width={200} style={{ background: colorBgContainer }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+            items={items2}
+          />
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+            className="h-screen"
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </>
   );
 };
 
